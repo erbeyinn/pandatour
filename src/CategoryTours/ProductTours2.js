6d8d948154549a88ad3549be6bd14f74608eb467
erbeyinn <78265795+erbeyinn@users.noreply.github.com>
2021-02-01 18:34:44 +0300

    er

--- src/CategoryTours/ProductTours2.js
+++ src/CategoryTours/ProductTours2.js
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 
 const ProductTours = () => {
 
-     const { t, i18n } = useTranslation();
+     const { t } = useTranslation();
     return (
       <div>
         
